81371997f73f5b9f29d56ec96f8d8878aa6da9ab
Satyaprakash <satyaprakash.in33@gmail.com>
2026-05-19 09:08:56 +0530

    fix api url for production

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL || '' });
 
 export const apiSlice = createApi({
   baseQuery,
